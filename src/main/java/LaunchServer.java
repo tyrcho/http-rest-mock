ffb99274738eb0f2440e28579ed4d5dd81876491
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-02-26 17:57:17 +0100

    :recycle: Minimalist sample.

--- src/main/java/LaunchServer.java
+++ src/main/java/LaunchServer.java
@@ -2,19 +2,15 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.process.Inflector;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.model.Resource;
+import org.glassfish.jersey.server.model.Resource.Builder;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class LaunchServer {
     public static void main(String[] args) {
-        SkyService skyService = mock(SkyService.class);
-        when(skyService.getMessage()).thenReturn("hello");
         URI uri = UriBuilder.fromPath("/")
                 .host("localhost")
                 .port(12345)
@@ -22,26 +18,28 @@ public class LaunchServer {
                 .build();
 
         final ResourceConfig rc = new ResourceConfig();
-        Resource.Builder builder = Resource.builder("/test");
-
-        builder.addChildResource("/hello")
-                .addMethod("GET")
-                .handledBy(new Inflector<ContainerRequestContext, Response>() {
-                    public Response apply(ContainerRequestContext context) {
-                        Object aaargs = context.getUriInfo().getQueryParameters().get("arg");
-                        return Response.ok("Hello " + aaargs).build();
-                    }
-                });
-
-        rc.registerResources(builder.build());
-//rc.register(skyService);
+        rc.registerResources(simpleResource());
         GrizzlyHttpServerFactory.createHttpServer(uri, rc, true);
 
 
     }
 
-    public interface SkyService {
-        String getMessage();
+    // test at http://localhost:12345/test/hello?arg=55
+    private static Resource simpleResource() {
+        Builder builder = Resource.builder("/test");
+
+        // cannot be replaced by a lambda, grizzly server won't handle request
+        Inflector<ContainerRequestContext, Object> inflector = new Inflector<ContainerRequestContext, Object>() {
+            @Override
+            public Object apply(ContainerRequestContext context) {
+                return Response.ok("Hello " + context.getUriInfo().getQueryParameters().get("arg")).build();
+            }
+        };
+        builder.addChildResource("/hello")
+                .addMethod("GET")
+                .handledBy(inflector);
+
+        return builder.build();
     }
 }
 
